787172e47ce1fb6505fa7ad9dac9e27b8278bf84
Daniel Hermansen <daniel.j.hermansen@gmail.com>
2020-06-09 19:34:34 -0600

    MVP complete

---
+++ api/server.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const session = require('express-session')
+const knexSessionStore = require('connect-session-knex')(session);
+const helmet = require("helmet");
+const cors = require("cors");
+const userRouter = require('../users/user-router.js')
+
+const server = express()
+
+const sessionConfig = {
+    name: 'anyNameforSession',
+    secret: 'shh',
+    cookie: {
+        maxAge: 60000, //Milliseconds
+        secure: false, // should be true in production
+        httpOnly: true //only for Http use
+    },
+    resave: false,
+    saveUninitialized: false,
+
+    store: new knexSessionStore(
+        {
+            knex: require('../data/dbConfig.js'),
+            tablename: "sessions",
+            sidfieldname: "sid",
+            createtable: true,
+            clearInterval: 60000
+        }
+    )
+}
+
+server.use(helmet());
+server.use(express.json());
+server.use(cors());
+//Use Session
+server.use(session(sessionConfig));
+
+server.use('/api/users', userRouter)
+
+server.get("/", (req, res) => {
+    res.send('Server is Running...');
+});
+
+module.exports = server;
